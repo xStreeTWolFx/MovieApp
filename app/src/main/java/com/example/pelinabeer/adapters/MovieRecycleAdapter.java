00b9bcef21141e8000fbb6ae1a8ea22f33406584
Reyni Thomas T <reyni.1210@gmail.com>
2020-09-07 01:01:50 -0400

    ✨ Crea adapter
        Crea el adapter que manejará las listas de peliculas

---
+++ app/src/main/java/com/example/pelinabeer/adapters/MovieRecycleAdapter.java
@@ -0,0 +1,56 @@
+package com.example.pelinabeer.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pelinabeer.R;
+import com.example.pelinabeer.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MovieRecycleAdapter extends RecyclerView.Adapter<MovieRecycleAdapter.ViewHolder> {
+    List<Movie> movieList;
+
+    public MovieRecycleAdapter(List<Movie> movieList) {
+        this.movieList = movieList;
+    }
+
+    public void setMovieList(List<Movie> movieList) {
+        this.movieList = movieList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Movie movie = movieList.get(position);
+        Picasso.get().load(movie.getPoster()).resize(400,400).placeholder(R.drawable.ic_star).centerInside()
+                .error(R.drawable.ic_launcher_foreground).into(holder.movieImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView movieImage;
+
+        ViewHolder(View view) {
+            super(view);
+            movieImage = view.findViewById(R.id.image_movie);
+        }
+    }
+}
