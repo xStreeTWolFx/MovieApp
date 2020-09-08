30e4f693f60b482ad7f8b38106ec8942d831d5b2
Reyni Thomas T <reyni.1210@gmail.com>
2020-09-07 20:21:17 -0400

    ✨ Crea metodo nuevo
        Agrega metodo de borrado

--- app/src/main/java/com/example/pelinabeer/database/dao/MovieDao.java
+++ app/src/main/java/com/example/pelinabeer/database/dao/MovieDao.java
@@ -2,6 +2,7 @@ package com.example.pelinabeer.database.dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -21,6 +22,9 @@ public interface MovieDao {
 
     @Query("DELETE FROM movie")
     void deleteAll();
+
+    @Delete
+    void deleteAll(Movie... movies);
 
     @Query("SELECT * from movie ORDER BY id ASC")
     LiveData<List<Movie>> getAllMovies();
