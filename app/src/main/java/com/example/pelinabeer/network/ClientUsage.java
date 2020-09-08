d43894c6e8670a80cebb6b32735878de10d5b875
Reyni Thomas T <reyni.1210@gmail.com>
2020-09-07 20:18:35 -0400

    ✨ Modifica instancia de retrofit
        Modifica el url base, y la manera de agregar el Bearer token del API.

--- app/src/main/java/com/example/pelinabeer/network/ClientUsage.java
+++ app/src/main/java/com/example/pelinabeer/network/ClientUsage.java
@@ -17,7 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ClientUsage {
 
-    private static final String API_URL = "https://api.themoviedb.org/";
+    private static final String API_URL = "https://api.themoviedb.org/3/";
     private static final String BEARER = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjI2NDlhNTZhNGVmMjlkYjY4Zjg3NzdkMzM1MmY5NCIsInN1YiI6IjViZTVhOWFkMGUwYTI2MTRiMTA0NTFlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gIOUhDUq1gvHaEoplGCIRR_xiA_XKiSACoec0CEghw4";
     private static Retrofit retrofit;
 
@@ -27,12 +27,9 @@ public class ClientUsage {
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         builder.addInterceptor(loggingInterceptor);
-        builder.addInterceptor(new Interceptor() {
-            @Override
-            public Response intercept(@NonNull Chain chain) throws IOException {
-                Request request = chain.request().newBuilder().addHeader("Bearer", BEARER).build();
-                return chain.proceed(request);
-            }
+        builder.addInterceptor(chain -> {
+            Request request = chain.request().newBuilder().header("Authorization","Bearer "+ BEARER).build();
+            return chain.proceed(request);
         });
         OkHttpClient client = builder.build();
         if (retrofit == null) {
